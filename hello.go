9e37ff3af3cfd74fb00af5b4a9f26864837fe1e2
gellanz <angelizarrarasg@gmail.com>
2025-07-19 21:59:52 +0000

    simple HandleFunc and file server

--- hello.go
+++ hello.go
@@ -1,7 +1,19 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+)
 
 func main() {
-	fmt.Println("Hello, Go in Docker Devcontainer!")
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Hello world, you've requested: %s \n", r.URL.Path)
+	})
+
+	fs := http.FileServer(http.Dir("static/"))
+
+	http.Handle("/static/", http.StripPrefix("/static/", fs))
+
+	http.ListenAndServe(":80", nil)
 }
